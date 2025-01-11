347d9b888510a598bb213f12fb5fd7ea03d966c6
Zalak Nrupay Shah <oel232ec@students.codeinstitute.net>
2025-01-11 16:32:53 +0000

    sql-expression

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -3,8 +3,13 @@ connection = psycopg2.connect(database = "chinook")
 
 
 cursor = connection.cursor() 
-cursor.execute('select * from "Artist"')
+# cursor.execute('select * from "Artist"')
+# cursor.execute('SELECT "Name" * FROM "Album"')
+#cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ['Queen'])
+ 
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 results = cursor.fetchall()
+# results = cursor.fetchone()
 connection.close()
 print(results)
 for result in results:
